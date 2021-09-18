8775a5a51660c0663c5f9bbf8d2dc749949ef8fa
Hamzah <82611798+brobro10000@users.noreply.github.com>
2021-09-17 20:53:20 -0400

    setting up for heroku

--- client/src/App.js
+++ client/src/App.js
@@ -3,12 +3,13 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  console.log("world Hello")
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Hello World
         </p>
         <a
           className="App-link"
