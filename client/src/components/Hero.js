90a50e246b707eca5e68537d4763db2aa9aaad6c
Luke Hampton <lukehampton6@gmail.com>
2021-09-21 17:16:57 -0400

    hero component with carousel

---
+++ client/src/components/Hero.js
@@ -0,0 +1,50 @@
+import { Carousel, Container } from "react-bootstrap";
+import strawberry from "../assets/images/strawberryhero.jpg";
+import orange from "../assets/images/orangehero.jpg";
+import peach from "../assets/images/peachhero.jpg";
+
+function Hero() {
+  return (
+    <Container>
+      <Carousel variant="dark">
+        <Carousel.Item interval={5000}>
+          <img
+            className="d-block w-100"
+            src={strawberry}
+            alt="First slide"
+          />
+          <Carousel.Caption>
+            <h3>First slide label</h3>
+            <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+          </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item interval={5000}>
+          <img
+            className="d-block w-100"
+            src={orange}
+            alt="First slide"
+          />
+          <Carousel.Caption>
+            <h3>Second slide label</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+          </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item interval={5000}>
+          <img
+            className="d-block w-100"
+            src={peach}
+            alt="First slide"
+          />
+          <Carousel.Caption>
+            <h3>Third slide label</h3>
+            <p>
+              Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+            </p>
+          </Carousel.Caption>
+        </Carousel.Item>
+      </Carousel>
+    </Container>
+  );
+}
+
+export default Hero;
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,8 +1,10 @@
 import Products from "../components/Products";
+import Hero from "../components/Hero";
 
 const Home = () => {
     return (
         <div>
+            <Hero />
             <Products />
         </div>
     )
