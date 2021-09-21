b1ef37521d879082b1bbf1cf343775bd55d7f5d2
Luke Hampton <lukehampton6@gmail.com>
2021-09-21 12:06:20 -0400

    apollo imports and graphql installed

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,43 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  createHttpLink,
+} from '@apollo/client';
+import { setContext } from '@apollo/client/link/context';
 import { Provider } from 'react-redux'
 import store from './utils/store'
 import Navigation from './components/Navigation';
+
+const httpLink = createHttpLink({
+  uri: '/graphql',
+});
+
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem('id_token');
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : '',
+    },
+  };
+});
+
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache(),
+});
+
 function App() {
   console.log(store)
   return (
+    <ApolloProvider client={client}>
     <Provider store={store}>
       <Navigation />
     </Provider>
+    </ApolloProvider>
   );
 }
 
--- client/src/components/Products.js
+++ client/src/components/Products.js
@@ -5,6 +5,11 @@ import { UPDATE_PRODUCTS } from '../utils/actions';
 import { QUERY_ALL_PRODUCTS } from '../utils/queries';
 
 function Products() {
+    const state = useSelector(state => state)
+    const dispatch = useDispatch()
+
+    const { loading, data } = useQuery(QUERY_ALL_PRODUCTS);
+
     return (
         <div></div>
     )
