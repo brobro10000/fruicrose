e5031321ca00f228c849a0e1bcfd1867afbab84c
Hamzah <82611798+brobro10000@users.noreply.github.com>
2021-09-21 19:23:28 -0400

    trying to fix heroku

--- client/src/index.js
+++ client/src/index.js
@@ -5,9 +5,9 @@ import App from './App';
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 
 ReactDOM.render(
-<React.StrictMode>
+
     <App />
-  </React.StrictMode>,
+  ,
   document.getElementById('root')
 );
 
