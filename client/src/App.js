7eeabff857f58cc7e196e9a13393319618ebe85d
Luke Hampton <lukehampton6@gmail.com>
2021-09-23 15:51:36 -0400

    single product component created

--- client/src/App.js
+++ client/src/App.js
@@ -14,6 +14,7 @@ import Navigation from "./components/Navigation";
 import Home from "./pages/Home";
 import Contact from "./pages/Contact";
 import Dashboard from "./pages/Dashboard";
+import Cart from "./pages/Cart";
 
 const httpLink = createHttpLink({
   uri: "http://localhost:3001/graphql",
