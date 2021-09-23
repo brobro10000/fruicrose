4b8799e40d12f7cc109b44a3a0a4b90554aed7a5
Hamzah <82611798+brobro10000@users.noreply.github.com>
2021-09-23 16:42:45 -0400

    Sort Functionality created

--- client/src/components/Products.js
+++ client/src/components/Products.js
@@ -4,6 +4,7 @@ import { useSelector, useDispatch } from "react-redux";
 import { UPDATE_PRODUCTS } from "../utils/actions";
 import { QUERY_ALL_PRODUCTS } from "../utils/queries";
 import { Image, Container, Card, Row, Button, ButtonGroup, Dropdown, Col } from "react-bootstrap";
+import { AiOutlineArrowDown, AiOutlineArrowUp } from 'react-icons/ai'
 import banana from '../assets/images/banana.jpeg'
 import blueberry from '../assets/images/blueberry.jpeg'
 import fujiapple from '../assets/images/fujiapple.jpg'
@@ -18,7 +19,7 @@ import watermelon from '../assets/images/watermelon.jpeg'
 
 function Products() {
   const fruitImages = [banana, blueberry, fujiapple, honeycrispapple, lemon, mango, peach, raspberry, tangerine, watermelon]
-  const sortByArr = ["Alphabetical"]
+  const sortByArr = ["Alphabetical", "Price"]
   const products = useSelector((state) => state.products);
   const [categoryList, updateCategoryList] = useState(0)
   const dispatch = useDispatch();
@@ -51,9 +52,47 @@ function Products() {
     })
     return updateCategoryList(allCategories)
   }, [products])
+
   function sortBy(e) {
-    console.log(e.target)
+    const sortedParam = e.target.innerHTML
+    var unsorted = []
+    const sortedProducts = []
+
+
+    if (sortedParam == sortByArr[0]) {
+      products.forEach(element => {
+        unsorted.push(element.name)
+      })
+      unsorted.sort()
+      unsorted.forEach(element => {
+        products.forEach(item => {
+          if (item.name === element) {
+            sortedProducts.push(item)
+          }
+        })
+      })
+    }
+    if (sortedParam == sortByArr[1]){
+      products.forEach(element => {
+        unsorted.push(element.price)
+      })
+      unsorted.sort()
+      unsorted.forEach(element => {
+        products.forEach(item => {
+          console.log(item,element)
+          if (item.price === element) {
+            sortedProducts.push(item)
+          }
+        })
+      })
+    }
+    dispatch({
+      type: UPDATE_PRODUCTS,
+      products: sortedProducts,
+    });
   }
+
+
   function filterItem(e) {
     const filterCategory = e.target.innerHTML
     if (filterCategory === 'Reset') {
@@ -74,35 +113,43 @@ function Products() {
   if (!products?.length) {
     return <h1>There are no products!</h1>;
   }
+
   return (
     <Container>
       <Row>
         <Col>
-      <Dropdown>
-        <Dropdown.Toggle id="dropdown-button-dark" variant="secondary">
-          Category
-        </Dropdown.Toggle>
-        <Dropdown.Menu variant="dark">
-          {categoryList ? (categoryList.map((product) => {
-            return (<Dropdown.Item key={product} onClick={filterItem} >{product}</Dropdown.Item>)
-          })) : null }
-          <Dropdown.Divider />
-          <Dropdown.Item onClick={filterItem} >Reset</Dropdown.Item>
-        </Dropdown.Menu>
-      </Dropdown>
-      </Col>
-      <Col>
-      <Dropdown>
-        <Dropdown.Toggle id="dropdown-button-dark" variant="secondary">
-          Sort By
-        </Dropdown.Toggle>
-        <Dropdown.Menu variant="dark">
-          {sortByArr ? (sortByArr.map((type) => {
-            return (<Dropdown.Item key={type} onClick={sortBy} >{type}</Dropdown.Item>)
-          })) : null }
-        </Dropdown.Menu>
-      </Dropdown>
-      </Col>
+          <Dropdown>
+            <Dropdown.Toggle id="dropdown-button-dark" variant="secondary">
+              Category
+            </Dropdown.Toggle>
+            <Dropdown.Menu variant="dark">
+              {categoryList ? (categoryList.map((product) => {
+                return (<Dropdown.Item key={product} onClick={filterItem} >{product}</Dropdown.Item>)
+              })) : null}
+              <Dropdown.Divider />
+              <Dropdown.Item onClick={filterItem}>Reset</Dropdown.Item>
+            </Dropdown.Menu>
+          </Dropdown>
+        </Col>
+        <Col>
+          <Dropdown>
+            <Dropdown.Toggle id="dropdown-button-dark" variant="secondary">
+              Sort By
+            </Dropdown.Toggle>
+            <Dropdown.Menu variant="dark">
+              {sortByArr ? (sortByArr.map((type) => {
+                return (
+                  <Dropdown.Item key={type} onClick={sortBy} >
+                    <Row>
+                      <Col>
+                        {type}
+                      </Col>
+                    </Row>
+                  </Dropdown.Item>)
+              })) : null}
+            </Dropdown.Menu>
+          </Dropdown>
+        </Col>
       </Row>
       <Row>
         {products.map((product) => {
