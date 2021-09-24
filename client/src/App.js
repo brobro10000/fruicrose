afcadde653c21a387c48bd97e4f8c7928a30960f
Hamzah <82611798+brobro10000@users.noreply.github.com>
2021-09-23 20:44:10 -0400

    quick changes

--- client/src/App.js
+++ client/src/App.js
@@ -17,7 +17,7 @@ import Dashboard from "./pages/Dashboard";
 import Cart from "./pages/Cart";
 
 const httpLink = createHttpLink({
-  uri: "/graphql",
+  uri: "http://localhost:3001/graphql",
 });
 
 const authLink = setContext((_, { headers }) => {
