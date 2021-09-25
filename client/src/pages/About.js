a0373f6c62a10dacee1cf3fb2236998606bcc4ad
Hamzah <82611798+brobro10000@users.noreply.github.com>
2021-09-25 12:32:56 -0400

    initial about created

---
+++ client/src/pages/About.js
@@ -0,0 +1,15 @@
+import {Container} from 'react-bootstrap'
+function About() {
+  return (
+    <Container>
+        
+        About About About About About About About About About About About About 
+        About About About About About About About About About About About About 
+        About About About About About About About About About About 
+        About About About About About About About About 
+        About About About About About About About About 
+    </Container>
+  );
+}
+
+export default About;
