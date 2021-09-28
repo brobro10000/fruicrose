0d041e92d95861230b08eb3edcc090f4082b0a45
Hamzah <82611798+brobro10000@users.noreply.github.com>
2021-09-28 12:14:33 -0400

    more logo stuff

--- client/src/pages/About.js
+++ client/src/pages/About.js
@@ -7,7 +7,7 @@ function About() {
          <Card id='aboutCard'>
                 <Row>
                     <Col md={12}>
-                    <Image id='aboutImage' alt='Amazingly Handsome Man' src={logo} fluid/>  
+                    <Image id='aboutImage' alt='Logo' src={logo} fluid/>  
                     </Col>
                     <Col id="aboutText" md={12}>
                         <Card.Title id='aboutTitle' >
@@ -25,15 +25,14 @@ function About() {
         <Card id='aboutCard'>
                 <Row>
                     <Col md={12}>
-                        <Image id='aboutImage' alt='Amazingly Handsome Man' src={botanist} fluid/>
+                        <Image id='aboutImage' alt='Distributer' src={botanist} fluid/>
                     </Col>
                     <Col id="aboutText" md={12}>
                         <Card.Title id='aboutTitle' >
-                            My name is Hamzah Ullah, and I love to code!
+                            Meet our Distributor! 
                         </Card.Title>
                         <Card.Body>
-                       <p> I am currently pursuing a Bachelor's Degree in Computer Engineering from the University of Central Florida with an emphasis on machine learning and robotics.
-                        I am outgoing, dedicated, and open-minded. I get across to people and adjust to changes with ease.</p>
+                       <p> The Botonist are a locally sourced distributor that provides us </p>
                         <p id='mission'> "Life is about the constant pursuit of knowledge, if you stop learning, you stop growing."</p>
                            <p>  Currently, I am looking for new career opportunities my current job position cannot provide.</p>
                         </Card.Body>
