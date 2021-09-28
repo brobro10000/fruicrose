4b4e16702180dffce37313d468ed045e24587ead
Hamzah <82611798+brobro10000@users.noreply.github.com>
2021-09-28 10:41:02 -0400

    Initial about page

--- client/src/pages/About.js
+++ client/src/pages/About.js
@@ -1,12 +1,26 @@
-import {Container} from 'react-bootstrap'
+import {Container, Card, Row, Col, Image} from 'react-bootstrap'
+import botanist from '../assets/images/Botanist.svg'
 function About() {
   return (
     <Container>
-        About About About About About About About About About About About About 
-        About About About About About About About About About About About About 
-        About About About About About About About About About About 
-        About About About About About About About About 
-        About About About About About About About About 
+        <Card id='aboutCard'>
+                <Row>
+                    <Col md={12}>
+                        <Image id='aboutImage' alt='Amazingly Handsome Man' src={botanist} fluid/>
+                    </Col>
+                    <Col id="aboutText" md={12}>
+                        <Card.Title id='aboutTitle' >
+                            My name is Hamzah Ullah, and I love to code!
+                        </Card.Title>
+                        <Card.Body>
+                       <p> I am currently pursuing a Bachelor's Degree in Computer Engineering from the University of Central Florida with an emphasis on machine learning and robotics.
+                        I am outgoing, dedicated, and open-minded. I get across to people and adjust to changes with ease.</p>
+                        <p id='mission'> "Life is about the constant pursuit of knowledge, if you stop learning, you stop growing."</p>
+                           <p>  Currently, I am looking for new career opportunities my current job position cannot provide.</p>
+                        </Card.Body>
+                    </Col>
+                </Row>
+            </Card>
     </Container>
   );
 }
