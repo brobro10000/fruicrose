8d75360deba301dc55958deceee829c60cfa400a
Hamzah <82611798+brobro10000@users.noreply.github.com>
2021-09-28 22:09:35 -0400

    fiuxed stripe

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -45,7 +45,7 @@ const typeDefs = gql`
     product(_id: ID): Product
     user: User
     order(_id: ID): Order
-    checkout(products: [ID]!, stock: [Int]!): Checkout
+    checkout(products: [ID]!, quantity: [Int]!): Checkout
   }
 
   type Mutation {
