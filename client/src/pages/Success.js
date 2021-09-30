9c3befd0c9084ef37a36d104cfab8e886748230c
luke <lukehampton6@gmail.com>
2021-09-30 17:25:03 -0400

    success page styling

--- client/src/pages/Success.js
+++ client/src/pages/Success.js
@@ -43,7 +43,8 @@ function Success() {
   }, [addOrder]);
 
   return (
-    <div>
+    <div className="centerContainer">
+    <div className="success newBorder">
       <h1>Success!</h1>
       <h2>Thank you for your purchase!</h2>
       <h2>You will now be redirected to the home page</h2>
@@ -52,6 +53,7 @@ function Success() {
       quantity={quantity}
         />
       <Loading />
+    </div>
     </div>
   );
 }
