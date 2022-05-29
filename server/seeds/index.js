3296b868e4b5c2293deb6a1644e1a5bc036ed883
Hamzah <82611798+brobro10000@users.noreply.github.com>
2022-05-29 12:59:30 -0400

    test

--- server/seeds/index.js
+++ server/seeds/index.js
@@ -38,6 +38,7 @@ db.once("open", async () => {
       description:
         "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
       price: 1.2,
+      imageLink: "..assets/images/honeycrispappleAVIF.avif",
       stock: 43,
       unit: "apple",
       categories: categories[0]._id,
